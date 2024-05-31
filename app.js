90b9c1b8df8624c2d0de7c16660b3c821f127e35
PriscillaPayet <priscilla.ppayet@gmail.com>
2024-05-31 11:46:42 +0200

    Initial commit

---
+++ app.js
@@ -0,0 +1,70 @@
+
+// Notre variable qui contient le "module" app (un objet)
+
+let app = {
+    
+    // Tableau contenant le nom des 4 maisons : je transforme le tableau en objet et je le sors de ma fonction
+    houses: [
+        'anthorvus',
+        'darioptera',
+        'lustrix',
+        'maxopus',
+    ],
+
+    // Méthode appelée au chargement de la page
+    init: function() {
+        //je sélectionne le formulaire contenant le champs
+        const form = document.querySelector("form");
+        //j'écoute la soumission et je vérifie si le prénom est ok
+        form.addEventListener("submit",app.handleSubmit,);
+    
+    },
+
+    handleSubmit: function(event){
+        //je bloque le rafraichissement
+        event.preventDefault();
+        //je sémectionne le champs 
+        const userNameField = document.querySelector(".name");
+        //je récupère la valeur soumise
+        const nameField = userNameField.value; 
+        //je vérifie si champs n'est pas vide ou n'est pas un nombre
+        if (nameField == "" || (isNaN(nameField)==false)){
+            alert ("Ça ne fonctionne qu'avec un nom, pardi !");
+            return false;
+        }
+
+        else {
+            //let speech = document.querySelector(".speech_container");
+            let divSpeech = document.querySelector(".speech");
+            divSpeech.remove();
+            console.log("texte supprimé?");
+            
+            let randomNumber = Math.floor(Math.random() * (3 - 0) + 0);
+            console.log(randomNumber);
+            let divImage = document.querySelector(".speech_container");
+            let houseImg = document.createElement("img");
+            divImage.appendChild(houseImg); 
+        
+            if (randomNumber == 0){
+            houseImg.src="images/anthorvus.png";
+            }
+            else if (randomNumber == 1){
+            houseImg.src="images/darioptera.png";
+            }
+            else if (randomNumber == 2){
+            houseImg.src="images/lustrix.png";
+            }
+            else {
+             houseImg.src = "images/maxopus.png";
+           }
+        }   
+        
+    },
+    
+
+
+    
+    
+}
+// Quand la page est entièrement chargée, on exécute la méthode init située dans l'object app.
+document.addEventListener('DOMContentLoaded', app.init);
---
+++ readme.md
@@ -0,0 +1,7 @@
+# Poudlock
+
+Il était une fois, une école d'apprentis magidevs du nom de Poudlock. Cette école a été fondée par 4 personnes aux talents divers et complémentaires : **Anthony**, **Dario**, **Lucie** et **Maxime**.
+
+Ces derniers décidèrent de diviser l'école en 4 maisons distinctes basées sur leurs noms. Pour ce faire, nos magidevs conçurent un habile système de sélection bâptisé le **choixp'O**. Toute ressemblance avec une oeuvre de fiction serait purement fortuite.
+
+
